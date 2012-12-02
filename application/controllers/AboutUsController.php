6319052ffb7f50b03b7a500bb7f8689713aadcc7
Chris Rogers <christopherdestiny@gmail.com>
2012-12-02 01:10:57 -0800

    adding images to static content

--- application/controllers/AboutUsController.php
+++ application/controllers/AboutUsController.php
@@ -15,12 +15,16 @@ class AboutUsController extends Zend_Controller_Action
     /**
      * The history page action.  
      */
-    public function historyAction() {}
+    public function historyAction() {
+        $this->view->getHelper('headLink')->appendStylesheet('/css/about-us/history.css');
+    }
     
     /**
      * The pastor's page action. 
      */
-    public function pastorsPageAction() {}
+    public function pastorsPageAction() {
+        $this->view->getHelper('headLink')->appendStylesheet('/css/about-us/pastors-page.css');
+    }
     
     /**
      * The contact us action. 
