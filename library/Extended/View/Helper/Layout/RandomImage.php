9872d2d20a6621fb24def5614eebb6cf0e99f0ef
mkosolofski <matthew_kosolofski@hotmail.com>
2012-12-08 22:12:29 -0800

    More IE fixes

--- library/Extended/View/Helper/Layout/RandomImage.php
+++ library/Extended/View/Helper/Layout/RandomImage.php
@@ -12,12 +12,12 @@ class Extended_View_Helper_Layout_RandomImage extends Zend_View_Helper_Abstract
      * The random images. 
      */
     static $_images = array(
-        'commandments.png',
-        'crosses.png',
-        'dove.png',
-        'hands.png',
-        'thorns.png',
-        'wine.png'
+        array('src' => '/images/layout/random/commandments.png', 'width' => '180px', 'height' => '171px'),
+        array('src' => '/images/layout/random/crosses.png', 'width' => '180px', 'height' => '176px'),
+        array('src' => '/images/layout/random/dove.png', 'width' => '180px', 'height' => '162px'),
+        array('src' => '/images/layout/random/hands.png', 'width' => '180px', 'height' => '190px'),
+        array('src' => '/images/layout/random/thorns.png', 'width' => '180px', 'height' => '77px'),
+        array('src' => '/images/layout/random/wine.png', 'width' => '180px', 'height' => '156px')
     );
 
     /**
@@ -27,7 +27,11 @@ class Extended_View_Helper_Layout_RandomImage extends Zend_View_Helper_Abstract
     {
         $imageIndex = mt_rand(0, count(self::$_images) - 1);
  
-        $html = '<img src="/images/layout/random/' . self::$_images[$imageIndex] . '"/>';
+        $html = '<img ';
+        foreach (self::$_images[$imageIndex] as $property => $value) {
+            $html .= $property .'="' . htmlentities($value) . '" '; 
+        }
+        $html .= '/>';
 
         // Don't display the same random image twice.
         unset(self::$_images[$imageIndex]);
