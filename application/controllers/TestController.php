da60b2cf49f86f34bf385bb30ae5d2260760220f
mkosolofski <matthew_kosolofski@hotmail.com>
2012-12-11 19:43:21 -0800

    Forgot to commit test controller in last commit.

---
+++ application/controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Contains TestController 
+ *
+ * @package Controllers
+ */
+
+/**
+ * The test controller.
+ * 
+ * @package Controllers
+ */
+class TestController extends Zend_Controller_Action
+{
+    /**
+     * The index page action.  
+     */
+    public function indexAction() {}
+}
